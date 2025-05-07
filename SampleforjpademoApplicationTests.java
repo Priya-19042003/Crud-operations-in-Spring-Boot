913ecd1e68744b650c7d56eb5bbc7a32c2d4562e
Priya-19042003 <158772137+Priya-19042003@users.noreply.github.com>
2025-05-07 17:45:58 +0530

    Add files via upload

---
+++ SampleforjpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleforjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleforjpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
